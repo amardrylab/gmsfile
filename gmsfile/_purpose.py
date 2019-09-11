9cc851852d423fa4625b1cae442dccfa02ac224d
Gourab Kanti Das <gourabkanti.das@visva-bharati.ac.in>
2019-09-11 05:17:15 +0000

    incorporation of IRC

--- gmsfile/_purpose.py
+++ gmsfile/_purpose.py
@@ -1,6 +1,6 @@
 class purpose:
 
-	def runtyp(self, purpose='energy'):
+	def runtyp(self, purpose='opt'):
 
 		energy=' $contrl runtyp=energy $end\n'
 
@@ -8,16 +8,16 @@ class purpose:
 		       ' $statpt opttol=0.0001 nstep=100 $end\n'
 
 		freq  =' $contrl runtyp=hessian $end\n'+ \
-		       ' $force method=analytic vibanal=.true. $end\n'+ \
-		       ' $statpt opttol=0.0001 nstep=30 $end\n'
+		       ' $force method=analytic vibanl=.true. $end\n'
 
-		irc   =' $contrl runtyp=irc $end\n'
-
+                sadpoint=' $contrl runtyp=sadpoint $end\n' +\
+                         " $force method=analytic $end\n" +\
+		         ' $statpt opttol=0.0001 nstep=30 hess=calc $end\n'
 
 		card= {
 			'ENERGY':energy,
 			'OPT'   :opt, 
 			'FREQ'  :freq, 
-			'IRC'   :irc,
+			'SADPOINT'   :sadpoint,
 			}
 		self.__purpose__= card[purpose.upper()]
